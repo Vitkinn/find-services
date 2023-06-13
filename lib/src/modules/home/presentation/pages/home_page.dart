ceb34b748246c530c8696bcd743c2332d3aad548
vitkinn <vhgmuller@gmail.com>
2023-06-12 23:41:27 -0300

    App custom SliverAppBar

--- lib/src/modules/home/presentation/pages/home_page.dart
+++ lib/src/modules/home/presentation/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tcc_frontend/src/modules/shared/components/footbar.dart';
+import 'package:tcc_frontend/src/modules/shared/components/sliver_app_bar.dart';
 import 'package:tcc_frontend/src/modules/shared/widgets/custom_text_field.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,241 +17,235 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        child: const CircleAvatar(),
-                      ),
-                      const Spacer(),
-                      GestureDetector(
-                        child: const Icon(Icons.chat_rounded),
-                        onTap: () => {},
-                      ),
-                    ],
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, __) => [
+          const CustomSliverAppBar(),
+        ],
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    controller: filterController,
+                    hintText: 'Pesquise por um serviço, prestador, etc...',
+                    obscureText: false,
                   ),
-                ),
-                const SizedBox(height: 20),
-                CustomTextField(
-                  controller: filterController,
-                  hintText: 'Pesquise por um serviço, prestador, etc...',
-                  obscureText: false,
-                ),
-                const SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      const Text(
-                        'Categorias',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Spacer(),
-                      GestureDetector(
-                        child: const Text(
-                          'Mais',
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        const Text(
+                          'Categorias',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
-                            color: Colors.blue,
                           ),
                         ),
-                      )
-                    ],
+                        const Spacer(),
+                        GestureDetector(
+                          child: const Text(
+                            'Mais',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 95,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 10,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        children: [
-                          Container(
-                            width: 75,
-                            height: 75,
-                            margin: const EdgeInsets.only(left: 20, bottom: 5),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              image: const DecorationImage(
-                                image: NetworkImage(
-                                    'https://images.twinkl.co.uk/tw1n/image/private/t_630/image_repo/2b/2b/T-T-17828-Editable-A4-Tools_ver_1.jpg'),
-                                fit: BoxFit.cover,
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 95,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            Container(
+                              width: 75,
+                              height: 75,
+                              margin:
+                                  const EdgeInsets.only(left: 20, bottom: 5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: const DecorationImage(
+                                  image: NetworkImage(
+                                      'https://images.twinkl.co.uk/tw1n/image/private/t_630/image_repo/2b/2b/T-T-17828-Editable-A4-Tools_ver_1.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                          ),
-                          Container(
-                            height: 15,
-                            margin: const EdgeInsets.only(left: 20),
-                            child: Text(
-                              'Categoria $index',
-                              textAlign: TextAlign.center,
+                            Container(
+                              height: 15,
+                              margin: const EdgeInsets.only(left: 20),
+                              child: Text(
+                                'Categoria $index',
+                                textAlign: TextAlign.center,
+                              ),
                             ),
-                          ),
-                        ],
-                      );
-                    },
+                          ],
+                        );
+                      },
+                    ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      const Text(
-                        'Recentes',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Spacer(),
-                      GestureDetector(
-                        child: const Text(
-                          'Mais',
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        const Text(
+                          'Recentes',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
-                            color: Colors.blue,
                           ),
                         ),
-                      )
-                    ],
+                        const Spacer(),
+                        GestureDetector(
+                          child: const Text(
+                            'Mais',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 95,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 10,
-                    itemBuilder: (BuildContext context, int index) {
-                      // const double defaultMargin = 20;
-                      // const double firstItemMargin = 20;
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 95,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        // const double defaultMargin = 20;
+                        // const double firstItemMargin = 20;
 
-                      // double margin =
-                      //     index == 0 ? firstItemMargin : defaultMargin;
-                      return Column(
-                        children: [
-                          Container(
-                            width: 75,
-                            height: 75,
-                            margin: const EdgeInsets.only(left: 20, bottom: 5),
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                    'https://cdn-icons-png.flaticon.com/512/4436/4436481.png'),
-                                fit: BoxFit.cover,
+                        // double margin =
+                        //     index == 0 ? firstItemMargin : defaultMargin;
+                        return Column(
+                          children: [
+                            Container(
+                              width: 75,
+                              height: 75,
+                              margin:
+                                  const EdgeInsets.only(left: 20, bottom: 5),
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      'https://cdn-icons-png.flaticon.com/512/4436/4436481.png'),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                          ),
-                          Container(
-                            height: 15,
-                            margin: const EdgeInsets.only(left: 20),
-                            child: Text(
-                              'Recente $index',
-                              textAlign: TextAlign.center,
+                            Container(
+                              height: 15,
+                              margin: const EdgeInsets.only(left: 20),
+                              child: Text(
+                                'Recente $index',
+                                textAlign: TextAlign.center,
+                              ),
                             ),
-                          ),
-                        ],
-                      );
-                    },
+                          ],
+                        );
+                      },
+                    ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      const Text(
-                        'Prestadores',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      GestureDetector(
-                        child: const Icon(Icons.filter_list_alt),
-                      ),
-                      const Spacer(),
-                      GestureDetector(
-                        child: const Text(
-                          'Mais',
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        const Text(
+                          'Prestadores',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
-                            color: Colors.blue,
                           ),
                         ),
-                      )
-                    ],
+                        const SizedBox(width: 10),
+                        GestureDetector(
+                          child: const Icon(Icons.filter_list_alt),
+                        ),
+                        const Spacer(),
+                        GestureDetector(
+                          child: const Text(
+                            'Mais',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 10,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          margin: const EdgeInsets.only(bottom: 20),
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 75,
-                                height: 75,
-                                margin: const EdgeInsets.only(right: 10),
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        'https://cdn-icons-png.flaticon.com/512/4436/4436481.png'),
-                                    fit: BoxFit.cover,
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 20),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 75,
+                                  height: 75,
+                                  margin: const EdgeInsets.only(right: 10),
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                          'https://cdn-icons-png.flaticon.com/512/4436/4436481.png'),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 252,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Prestador $index',
-                                    ),
-                                    const Text(
-                                      'Informações sobre o prestador deverão constar aqui.',
-                                    ),
-                                  ],
+                                SizedBox(
+                                  width: 252,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Prestador $index',
+                                      ),
+                                      const Text(
+                                        'Informações sobre o prestador deverão constar aqui.',
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              const Spacer(),
-                              const Icon(Icons.star_border),
-                            ],
+                                const Spacer(),
+                                const Icon(Icons.star_border),
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
