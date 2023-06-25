6129b9b0ac9dee82ee67c7bd23b4b23e87a8d221
vitkinn <vhgmuller@gmail.com>
2023-06-25 11:44:18 -0300

    APP footbar upgrade

--- lib/src/modules/shared/components/list_panel.dart
+++ lib/src/modules/shared/components/list_panel.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ListPanel extends StatelessWidget {
   final String title;
   final bool moreOptionVisibility;
-  final dynamic item;
+  final List? item;
   final VoidCallback? moreOptionOnTap;
   final VoidCallback? itemOnTap;
 
@@ -56,9 +56,30 @@ class ListPanel extends StatelessWidget {
             physics: const BouncingScrollPhysics(),
             itemCount: 200,
             itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 20, bottom: 5),
-                child: item,
+              return Column(
+                children: [
+                  Container(
+                    width: 75,
+                    height: 75,
+                    margin: const EdgeInsets.only(left: 20, bottom: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            'https://images.twinkl.co.uk/tw1n/image/private/t_630/image_repo/2b/2b/T-T-17828-Editable-A4-Tools_ver_1.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 15,
+                    margin: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Categoria $index',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
               );
             },
           ),
